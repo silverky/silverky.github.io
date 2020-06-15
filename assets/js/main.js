8483beb349b75e3b5c2a57634f16ddbfd586d976
dungbv <dsilverky@gmail.com>
2020-06-15 10:42:34 +0700

    Blog update at 2020-06-15 03:42:34

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 jQuery(document).ready(function($) {
 
     var config = {
-        'content-api-host': 'https://silverky.github.io',
+        'content-api-host': 'http://localhost:2368',
         'content-api-key': '372742ceab6e7be24f081e9eb3',
 	};
 	
